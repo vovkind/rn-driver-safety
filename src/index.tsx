f56779dc1518b4ea295cecfdf96b2a0df083cfb6
Vladimir B <vladimir.buchgalter@dragontailsystems.com>
2020-09-17 16:35:01 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { NativeModules } from 'react-native';
 
 type RnDriverSafetyType = {
   multiply(a: number, b: number): Promise<number>;
+  enable(a:  string): any;
 };
 
 const { RnDriverSafety } = NativeModules;
