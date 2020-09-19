8af0aa95c35093ece2dc2bb978db2eac58f2a67e
Vladimir B <vladimir.buchgalter@dragontailsystems.com>
2020-09-19 11:45:40 +0300

    add ts method enable

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -1,5 +1,6 @@
 declare type RnDriverSafetyType = {
     multiply(a: number, b: number): Promise<number>;
+    enable(a: string): any;
 };
 declare const _default: RnDriverSafetyType;
 export default _default;
